576dc2318bfc45b5592fe41358caabddf27385d4
Tonis Tiigi <tonistiigi@gmail.com>
2018-10-03 17:17:14 -0700

    add export to stack
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- build.go
+++ build.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	"github.com/moby/buildkit/client/llb"
+	"github.com/moby/buildkit/exporter/containerimage/exptypes"
 	"github.com/moby/buildkit/frontend/gateway/client"
 	"github.com/moby/buildkit/solver/pb"
 	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -21,35 +23,61 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 		stack = v
 	}
 
-	imageName, err := builderImageName(stack)
+	buildName, runName, err := builderImageName(stack)
 	if err != nil {
 		return nil, err
 	}
 
+	// TODO: read buildpacks and download directly
+
+	// TODO: git/http sources
 	src := llb.Local("context", llb.SessionID(c.BuildOpts().SessionID), llb.SharedKeyHint("pack-src"))
 
-	builderImage := llb.Image(imageName, llb.WithMetaResolver(c))
+	builderImage := llb.Image(buildName, llb.WithMetaResolver(c))
 
 	build := runBuilder(c, builderImage, `/packs/builder -buildpacksDir /var/lib/buildpacks  -outputDroplet /out/droplet.tgz -outputMetadata /out/result.json`, llb.Dir("/workspace"))
 	build.AddMount("/workspace", src, llb.Readonly)
 	build.AddMount("/tmp/cache", llb.Scratch(), llb.AsPersistentCacheDir("buildpack-build-cache", llb.CacheMountShared))
 
-	extract := llb.Image("alpine").Run(llb.Shlex("cp -Rf /in/. /out/"))
+	extract := llb.Image("alpine").Run(llb.Shlex(`sh -c "mkdir -p /out/home/vcap && tar -C /out/home/vcap -xzf /in/droplet.tgz && chown -R 2000:2000 /out/home/vcap"`), llb.WithCustomName("copy droplet to stack"), llb.Dir("/in"))
 
 	extract.AddMount("/in", build.Root(), llb.SourcePath("out"), llb.Readonly)
-	st := extract.AddMount("/out", llb.Scratch())
+	st := extract.AddMount("/out", llb.Image(runName))
 
 	def, err := st.Marshal()
 	if err != nil {
 		return nil, err
 	}
 
-	res, err := c.Solve(ctx, client.SolveRequest{
-		Definition: def.ToPB(),
+	eg, ctx := errgroup.WithContext(ctx)
+
+	var res *client.Result
+	eg.Go(func() error {
+		r, err := c.Solve(ctx, client.SolveRequest{
+			Definition: def.ToPB(),
+		})
+		if err != nil {
+			return err
+		}
+		res = r
+		return nil
 	})
-	if err != nil {
+
+	var config []byte
+	eg.Go(func() error {
+		_, c, err := c.ResolveImageConfig(ctx, runName, client.ResolveImageConfigOpt{})
+		if err != nil {
+			return err
+		}
+		config = c
+		return nil
+	})
+
+	if err := eg.Wait(); err != nil {
 		return nil, err
 	}
+	// TODO: is the build label needed?
+	res.AddMeta(exptypes.ExporterImageConfigKey, config)
 
 	return res, nil
 }
@@ -79,11 +107,11 @@ func runBuilder(c client.Client, img llb.State, cmd string, opts ...llb.RunOptio
 	return es
 }
 
-func builderImageName(stack string) (string, error) {
+func builderImageName(stack string) (string, string, error) {
 	switch stack {
 	case "cflinuxfs2":
-		return "packs/cflinuxfs2:build", nil
+		return "docker.io/packs/cflinuxfs2:build", "docker.io/packs/cflinuxfs2:run", nil
 	default:
-		return "", errors.Errorf("unsupported stack %s", stack)
+		return "", "", errors.Errorf("unsupported stack %s", stack)
 	}
 }
